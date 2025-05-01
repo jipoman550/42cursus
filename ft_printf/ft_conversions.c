d47dccf081f6aa6adf73cbc6f1d08f48f1df75e8
jipoman550 <sionsbed2@gmail.com>
2025-05-01 18:46:15 +0900

    ft_printf ver. 3

--- ft_printf/ft_conversions.c
+++ ft_printf/ft_conversions.c
@@ -6,7 +6,7 @@
 /*   By: sisung <sisung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 16:58:08 by sisung            #+#    #+#             */
-/*   Updated: 2025/05/01 18:33:03 by sisung           ###   ########.fr       */
+/*   Updated: 2025/05/01 18:45:15 by sisung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,11 @@ int	handle_conversion(char conversion, va_list args)
 	else if (conversion == 'i')
 		return convert_integer(args);
 	else if (conversion == 'u')
-		return convert_unsigned_decimal(args);
+		return convert_unsigned(args);
 	else if (conversion == 'x')
-		return convert_hexadecimal_lowercase(args);
+		return convert_hex_lower(args);
 	else if (conversion == 'X')
-		return convert_hexadecimal_uppercase(args);
+		return convert_hex_upper(args);
 	else if (conversion == '%')
 		return convert_percent();
 	return (0);
