895e7f8c308a3522f5d27f0a1baae30fcd0ac7df
jipoman550 <sionsbed2@gmail.com>
2025-05-10 19:22:33 +0900

    compeletion

--- ft_printf/ft_convert_string.c
+++ ft_printf/ft_convert_string.c
@@ -6,7 +6,7 @@
 /*   By: sisung <sisung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 17:25:25 by sisung            #+#    #+#             */
-/*   Updated: 2025/05/09 17:19:29 by sisung           ###   ########.fr       */
+/*   Updated: 2025/05/10 17:34:15 by sisung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,17 @@ int	convert_string(va_list args)
 	int		len;
 
 	s = va_arg(args, char *);
-	len = ft_strlen(s);
-	ft_putstr_fd(s, 1);
+	if (s == NULL)
+	{
+		ft_putstr_fd("(null)", 1);
+		len = 6;
+	}
+	else
+	{
+		len = ft_strlen(s);
+		ft_putstr_fd(s, 1);
+	}
 	return (len);
 }
 
-// !s 일 때도 처리를 해줘야하나?
+// !s 일 때도 처리를 해줘야하나? 그래ㅐ
