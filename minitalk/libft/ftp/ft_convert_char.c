fa56b48c98464de21b4a433d6df565e8718fdeb0
jipoman550 <sionsbed2@gmail.com>
2025-09-02 15:31:05 +0900

    [minitalk] feat(libft): add libft as a submodul

---
+++ minitalk/libft/ftp/ft_convert_char.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_char.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sisung <sisung@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/01 18:38:45 by sisung            #+#    #+#             */
+/*   Updated: 2025/06/03 09:45:17 by sisung           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include "libft.h"
+
+int	convert_char(va_list args)
+{
+	char	c;
+
+	c = (char)va_arg(args, int);
+	return (ftp_putchar_fd(c, 1));
+}
---
+++ minitalk/libft/ftp/ft_utils.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sisung <sisung@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/09 15:33:09 by sisung            #+#    #+#             */
+/*   Updated: 2025/06/03 09:46:20 by sisung           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include "./libft/libft.h"
+
+int	decimal_len(int num)
+{
+	int	digits;
+
+	digits = 1;
+	if (num < 0)
+		digits++;
+	while (num / 10)
+	{
+		digits++;
+		num /= 10;
+	}
+	return (digits);
+}
+
+int	unsigned_len(unsigned int num)
+{
+	int	digits;
+
+	digits = 1;
+	while (num / 10)
+	{
+		digits++;
+		num /= 10;
+	}
+	return (digits);
+}
+
+int	ft_putnbr_unsigned_fd(unsigned int n, int fd)
+{
+	char	digit;
+
+	if (fd < 0)
+		return (0);
+	if (n >= 10)
+		ftp_putnbr_fd(n / 10, fd);
+	digit = (n % 10) + '0';
+	if (write(fd, &digit, 1) == -1)
+		return (-1);
+	return (0);
+}
+
+int	hex_len(unsigned long num)
+{
+	int	digits;
+
+	digits = 1;
+	while (num / 16)
+	{
+		digits++;
+		num /= 16;
+	}
+	return (digits);
+}
+
+int	print_hex(unsigned long num, int is_upper)
+{
+	char	*hex_base;
+
+	if (is_upper)
+		hex_base = "0123456789ABCDEF";
+	else
+		hex_base = "0123456789abcdef";
+	if (num >= 16)
+		print_hex(num / 16, is_upper);
+	if (ftp_putchar_fd(hex_base[num % 16], 1) == -1)
+		return (-1);
+	return (0);
+}
