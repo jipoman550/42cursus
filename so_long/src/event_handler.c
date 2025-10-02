7e698ba72b127997ab6de4b33795afb9637f48c5
jipoman550 <sionsbed2@gmail.com>
2025-10-02 16:43:20 +0900

    [so_long] wip: save state with exit logic bugs (premature exit, P disappears)

--- so_long/src/event_handler.c
+++ so_long/src/event_handler.c
@@ -6,7 +6,7 @@
 /*   By: sisung <sisung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 14:32:26 by sisung            #+#    #+#             */
-/*   Updated: 2025/10/02 16:01:43 by sisung           ###   ########.fr       */
+/*   Updated: 2025/10/02 16:35:44 by sisung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	handle_exit(t_game *game)
 int	handle_keypress(int	keycode, t_game *game)
 {
 	ft_printf("x: %d, y: %d\n", game->player_x, game->player_y);
-
+	ft_printf("collectibles: %d\n", game->collectables);
 	if (keycode == KEY_ESC)
 		handle_exit(game);
 	else if (keycode == KEY_W || keycode == KEY_A
