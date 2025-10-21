9449d67bc2693d13981f4591be11da7844d1b020
jipoman550 <sionsbed2@gmail.com>
2025-10-21 17:52:32 +0900

    [philo] WIP feat(init): implement t_data and shared resource initialization logic

--- philosopher/philo/init.c
+++ philosopher/philo/init.c
@@ -6,31 +6,23 @@
 /*   By: sisung <sisung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 12:59:08 by sisung            #+#    #+#             */
-/*   Updated: 2025/10/20 09:18:35 by sisung           ###   ########.fr       */
+/*   Updated: 2025/10/21 12:15:46 by sisung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-t_data *init_data(char **argv, int argc)
+static int  parse_and_save_args(t_data *data, char **argv, int argc)
 {
-    t_data		*data;
     long long	val;
     size_t		i;
 
-    data = (t_data *)malloc(sizeof(t_data));
-    if (!data)
-        return (NULL);
     i = 1;
     while (i <= 4)
     {
         val = ft_parse_long(argv[i]);
         if (val == -1)
-        {
-            free(data);
-            printf("Error: Invalid argument value.\n");
-            return (NULL);
-        }
+            return (-1);
         if (i == 1) data->num_of_philos = (size_t)val;
         else if (i == 2) data->time_to_die = val;
         else if (i == 3) data->time_to_eat = val;
@@ -42,9 +34,8 @@ t_data *init_data(char **argv, int argc)
         val = ft_parse_long(argv[5]);
         if (val == -1)
         {
-            free(data);
             printf("Error: Invalid argument value for must_eat_count.\n");
-            return (NULL);
+            return (-1);
         }
         data->must_eat_count = (size_t)val;
     }
@@ -52,5 +43,32 @@ t_data *init_data(char **argv, int argc)
     {
         data->must_eat_count = 0;
     }
+    return (0);
+}
+
+t_data *init_data(char **argv, int argc)
+{
+    t_data *    data;
+    long long	val;
+    size_t		i;
+
+    data = (t_data *)malloc(sizeof(t_data));
+    if (!data)
+        return (NULL);
+
+    if (parse_and_save_args(data, argv, argc) != 0)
+    {
+        free(data);
+        printf("Error: Invalid argument value.\n");
+        return (NULL);
+    }
+
+    if (init_forks(data) != 0)
+    {
+        free(data);
+        return (NULL);
+    }
+
+    
     return (data);
 }
--- philosopher/philo/parse.c
+++ philosopher/philo/parse.c
@@ -6,7 +6,7 @@
 /*   By: sisung <sisung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 12:58:57 by sisung            #+#    #+#             */
-/*   Updated: 2025/10/20 09:18:33 by sisung           ###   ########.fr       */
+/*   Updated: 2025/10/21 10:24:11 by sisung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ static bool is_valid_digit(char *str)
 	return (true);
 }
 
-static long long	ft_atoll_safe(const char *str, bool *is_overflow)
+static long long	ft_atol_safe(const char *str, bool *is_overflow)
 {
 	long long	result;
 
