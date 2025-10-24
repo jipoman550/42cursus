2b796ffd449490bbe9ad7217f79b461dc3514b0b
Sion Sung <sisung@c1r20s8.42gyeongsan.kr>
2025-10-24 20:07:54 +0900

    [ft_printf] test: add temporary test file for debugging

---
+++ ft_printf/tests/main__.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("%%%%%%");
+	printf("\n");
+
+	printf("hi %%, I'm %%");
+	printf("\n");
+
+	printf("%%%%dd%%d%d", 42);
+	printf("\n");
+
+    return 0;
+}
