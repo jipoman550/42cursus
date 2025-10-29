c5fc85bff6c51587affba02945cd91edbac9f897
jipoman550 <sionsbed2@gmail.com>
2025-10-30 00:03:15 +0900

    [exam03] feat: update n_queens solution

--- exam3/pratice_exam03/n_queens/solution/n_queens.c
+++ exam3/pratice_exam03/n_queens/solution/n_queens.c
@@ -1,24 +1,51 @@
 #include "n_queens.h"
 
 // 4. print_solution(pos, N)
+void print_solution(const int *pos, int n)
+{
+	for (int i = 0; i < n; ++i)
+	{
+		if (i > 0)
+			fprintf(stdout, " ");
+
+		fprintf(stdout, "%d", pos[i]);
+	}
+	fprintf(stdout, "\n");
+}
+
 // 3. is_safe(pos, col, row)
+int is_safe(const int *pos, int col, int row)
+{
+	for (int i = 0; i < col; ++i)
+	{
+		if (pos[i] == row)
+			return 0;
+
+		int diff = pos[i] - row;
+		if (diff < 0)
+			diff = -diff;
+
+		if (diff == (col - i))
+			return 0;
+	}
+	return 1;
+}
+
 // 2. place_queens(pos, N, col)
 void place_queens(int *pos, int n, int col)
 {
-	int row;
-
 	if (col == n)
 	{
 		print_solution((const int *)pos, n);
 		return;
 	}
 
-	for (row = 0; row < n; ++row)
+	for (int row = 0; row < n; ++row)
 	{
 		if (is_safe(pos, col, row))
 		{
 			pos[col] = row;
-			place_queens(pos, n, col + 1)
+			place_queens(pos, n, col + 1);
 		}
 	}
 }
