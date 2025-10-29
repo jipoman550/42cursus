64ef5d17263254e2daf71237a1ea10f44a754d71
jipoman550 <sionsbed2@gmail.com>
2025-10-29 19:31:21 +0900

    [exam03] WIP: update n_queens solution of .c and .h file

--- exam3/pratice_exam03/n_queens/solution/n_queens.c
+++ exam3/pratice_exam03/n_queens/solution/n_queens.c
@@ -1,4 +1,47 @@
+#include "n_queens.h"
+
 // 4. print_solution(pos, N)
 // 3. is_safe(pos, col, row)
 // 2. place_queens(pos, N, col)
+void place_queens(int *pos, int n, int col)
+{
+	int row;
+
+	if (col == n)
+	{
+		print_solution((const int *)pos, n);
+		return;
+	}
+
+	for (row = 0; row < n; ++row)
+	{
+		if (is_safe(pos, col, row))
+		{
+			pos[col] = row;
+			place_queens(pos, n, col + 1)
+		}
+	}
+}
+
 // 1. main(argc, argv)
+int main(int argc, char **argv)
+{
+	int n;
+	int *pos;
+
+	if (argc != 2)
+		return 1;
+
+	n = atoi(argv[1]);
+	if (n <= 0)
+		return 1;
+
+	pos = (int *)calloc(n, sizeof(int));
+	if (pos == NULL)
+		return 1;
+
+	place_queens(pos, n, 0);
+
+	free(pos);
+	return 0;
+}
--- exam3/pratice_exam03/n_queens/solution/n_queens.h
+++ exam3/pratice_exam03/n_queens/solution/n_queens.h
@@ -1,7 +1,8 @@
 #ifndef N_QUEENS_H
 #define N_QUEENS_H
 
-
+#include <stdlib.h>
+#include <stdio.h>
 
 
 #endif
