f7385418d4e1aa4ad32d9d662f603c4579e3202b
jipoman550 <sionsbed2@gmail.com>
2026-01-24 12:58:34 +0900

    modify ft_popen

--- exam04/level_1/ft_popen/ft_popen.c
+++ exam04/level_1/ft_popen/ft_popen.c
@@ -1,51 +1,48 @@
-#include <unistd.h> // For pipe, fork, close, dup2, execvp
-#include <stdio.h>  // For general I/O and NULL
 #include <stdlib.h> // For exit
+#include <unistd.h> // For pipe, fork, close, dup2, execvp
 
 // Corrected function name and added necessary includes
 int ft_popen(const char *file, char *const argv[], char type)
 {
-    int fds[2];
+	int fds[2];
 
-    if (type != 'r' && type != 'w')
-        return (-1);
-    if (pipe(fds) < 0)
-        return (-1);
+	if (type != 'r' && type != 'w')
+		return (-1);
+	if (pipe(fds) < 0)
+		return (-1);
 
-    int pid = fork();
-    if (pid < 0)
-    {
-        close(fds[0]);
-        close(fds[1]);
-        return (-1);
-    }
+	int pid = fork();
+	if (pid < 0) {
+		close(fds[0]);
+		close(fds[1]);
+		return (-1);
+	}
 
-    if (pid == 0) // Child process
-    {
-        if (type == 'r') {
-            // Redirect stdout of the command to the pipe's write end
-            dup2(fds[1], STDOUT_FILENO); // Use STDOUT_FILENO for clarity
-        } else { // type == 'w'
-            // Redirect stdin of the command from the pipe's read end
-            dup2(fds[0], STDIN_FILENO); // Use STDIN_FILENO for clarity
-        }
-        close(fds[0]);
-        close(fds[1]);
-        execvp(file, argv);
-        // If execvp fails, exit with an error
-        exit(EXIT_FAILURE); // Use EXIT_FAILURE for standard practice
-    }
-    else // Parent process
-    {
-        if (type == 'r')
-        {
-            close(fds[1]); // Close the write end, parent doesn't need it for reading
-            return (fds[0]); // Return the read end of the pipe
-        }
-        else // type == 'w'
-        {
-            close(fds[0]); // Close the read end, parent doesn't need it for writing
-            return (fds[1]); // Return the write end of the pipe
-        }
-    }
+	if (pid == 0) // Child process
+	{
+		if (type == 'r') {
+			// Redirect stdout of the command to the pipe's write end
+			dup2(fds[1], STDOUT_FILENO); // Use STDOUT_FILENO for clarity
+		} else {                       // type == 'w'
+			// Redirect stdin of the command from the pipe's read end
+			dup2(fds[0], STDIN_FILENO); // Use STDIN_FILENO for clarity
+		}
+		close(fds[0]);
+		close(fds[1]);
+		execvp(file, argv);
+		// If execvp fails, exit with an error
+		exit(EXIT_FAILURE); // Use EXIT_FAILURE for standard practice
+	}
+	else                // Parent process
+	{
+		if (type == 'r') {
+			close(fds[1]); // Close the write end, parent doesn't need it for reading
+			return (fds[0]); // Return the read end of the pipe
+		}
+		else             // type == 'w'
+		{
+			close(fds[0]);   // Close the read end, parent doesn't need it for writing
+			return (fds[1]); // Return the write end of the pipe
+		}
+	}
 }
--- exam04/level_1/ft_popen/main.c
+++ exam04/level_1/ft_popen/main.c
@@ -6,38 +6,38 @@
 int ft_popen(const char *command, char *const argv[], char type);
 
 int main() {
-    // 'r' mode test
-    // Corrected array initialization syntax
-    char *args_ls[] = {"ls", "-l", NULL}; // Renamed to avoid conflict
-    int fd_r = ft_popen("ls", args_ls, 'r'); // ls -l의 결과를 읽을 수 있는 FD를 받음
+	// 'r' mode test
+	// Corrected array initialization syntax
+	char *args_ls[] = {"ls", "-l", NULL}; // Renamed to avoid conflict
+	int fd_r = ft_popen("ls", args_ls, 'r'); // ls -l의 결과를 읽을 수 있는 FD를 받음
 
-    if (fd_r == -1) {
-        perror("ft_popen for 'ls' failed");
-        return (1);
-    }
+	if (fd_r == -1) {
+		perror("ft_popen for 'ls' failed");
+		return (1);
+	}
 
-    char buf[1024];
-    int n;
-    // Read from the file descriptor returned by ft_popen
-    while ((n = read(fd_r, buf, sizeof(buf) - 1)) > 0) { // Use sizeof(buf)-1 for null termination
-        buf[n] = '\0';
-        printf("My program read: %s", buf);
-    }
-    close(fd_r); // Close the read file descriptor
+	char buf[1024];
+	int n;
+	// Read from the file descriptor returned by ft_popen
+	while ((n = read(fd_r, buf, sizeof(buf) - 1)) > 0) { // Use sizeof(buf)-1 for null termination
+		buf[n] = '\0';
+		printf("My program read: %s", buf);
+	}
+	close(fd_r); // Close the read file descriptor
 
-    // 'w' mode test
-    // Corrected array initialization syntax
-    char *args_grep[] = {"grep", "hello", NULL}; // Renamed to avoid conflict
-    int fd_w = ft_popen("grep", args_grep, 'w'); // grep의 입력으로 들어가는 FD를 받음
+	// 'w' mode test
+	// Corrected array initialization syntax
+	char *args_grep[] = {"grep", "hello", NULL}; // Renamed to avoid conflict
+	int fd_w = ft_popen("grep", args_grep, 'w'); // grep의 입력으로 들어가는 FD를 받음
 
-    if (fd_w == -1) {
-        perror("ft_popen for 'grep' failed");
-        return (1);
-    }
+	if (fd_w == -1) {
+		perror("ft_popen for 'grep' failed");
+		return (1);
+	}
 
-    write(fd_w, "hello world\n", 12);
-    write(fd_w, "this is test\n", 13); // 'hello'가 없으므로 출력되지 않아야 함
-    close(fd_w); // Close the write file descriptor
+	write(fd_w, "hello world\n", 12);
+	write(fd_w, "this is test\n", 13); // 'hello'가 없으므로 출력되지 않아야 함
+	close(fd_w); // Close the write file descriptor
 
-    return (0);
+	return (0);
 }
