8169854f1e82db7061011bbcb8845eef51d41fd7
jipoman550 <sionsbed2@gmail.com>
2026-02-18 18:20:40 +0900

    refactor: Rework ft_popen process and pipe handling
    - Add sys/wait.h include for process management
    - Standardize error checks for pipe and fork operations
    - Restructure child and parent process execution flow
    - Adjust pipe descriptor closing in parent and child processes
    - Introduce an argv iteration loop around fork and execvp calls
    - Change exit status from EXIT_FAILURE to 1

--- exam04/level_1/ft_popen/test/ft_popen.c
+++ exam04/level_1/ft_popen/test/ft_popen.c
@@ -1,45 +1,47 @@
+#include <sys/wait.h>
 #include <unistd.h>
-#include <stdlib.h>
 
 int ft_popen(const char *file, char *const argv[], char type)
 {
 	int fds[2];
+	pid_t pid;
 
-	if (type != 'r' && type != 'w')
-		return (-1);
-	if (pipe(fds) < 0)
+	if (pipe(fds) == -1)
 		return (-1);
 
-	int pid = fork();
-	if (pid < 0)
+	pid = fork();
+	if (pid == -1)
 	{
 		close(fds[0]);
 		close(fds[1]);
 		return (-1);
 	}
 
-	if (pid == 0)
+	while (argv[i])
 	{
-		if (type == 'r')
-			dup2(fds[1], STDOUT_FILENO);
-		else
-			dup2(fds[0], STDIN_FILENO);
-		close(fds[0]);
-		close(fds[1]);
-		execvp(file, argv);
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		if (type == 'r')
+		if (pid == 0)
 		{
-			close(fds[1]);
-			return (fds[0]);
+			if (type == 'r')
+			{
+				dup2(fds[1], STDOUT_FILENO);
+				close(fds[1]);
+			}
+			else
+			{
+				dup2(fds[0], STDIN_FILENO);
+				close(fds[0]);
+			}
+			execvp(file, argv[i]);
+			exit(1);
 		}
 		else
 		{
-			close(fds[0]);
-			return (fds[1]);
+			if (type == 'r')
+				close(fds[0]);
+			else
+				close(fds[1]);
 		}
+		i++;
 	}
+
 }
